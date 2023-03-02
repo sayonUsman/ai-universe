f4bcb282c6ee61656e0279d5c8f6a97a514d0809
Sayon Usman <sayonusman.ma@gmail.com>
2023-03-02 22:01:54 +0300

    Changed url

--- aiUniverseHub.js
+++ aiUniverseHub.js
@@ -1,38 +1,41 @@
-const loadInformation = async (id) => {
+const loadInformation = async () => {
   try {
-    const url = `https://openapi.programming-hero.com/api/ai/tool/0${id}`;
+    const url = `https://openapi.programming-hero.com/api/ai/tools`;
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data.data);
-    displayInformation(data.data);
+    console.log(data.data.tools);
+    displayInformation(data.data.tools);
   } catch (error) {
     console.log(error);
   }
 };
 
-const displayInformation = (mainData) => {
+const displayInformation = (information) => {
   const informationContainer = document.getElementById("information-container");
-  const informationSection = document.createElement("div");
-  let imageLink = "";
 
-  informationSection.innerHTML = `
+  for (let index = 0; index < 6; index++) {
+    const informationSection = document.createElement("div");
+    console.log(information[index]);
+
+    informationSection.innerHTML = `
       <div class="card h-100">
-        <img src="${mainData.image_link[0]}" class="img-fluid p-3" alt="There is a image.">
+        <img src="${information[index].image}" class="img-fluid p-3" alt="There is a image.">
 
         <div class="card-body">
           <h2 class="card-title fw-bold mb-3">Features</h2>
-          <p>1. ${mainData.features[1].feature_name}</p>
-          <p>2. ${mainData.features[2].feature_name}</p>
-          <p>3. ${mainData.features[3].feature_name}</p>
+          <p>1. ${information[index].features[0]}</p>
+          <p>2. ${information[index].features[1]}</p>
+          <p>3. ${information[index].features[2]}</p>
           <hr class="ps-3 pe-3"/>
-          <h2 class="card-title fw-bold mb-3">${mainData.tool_name}</h2>
+          <h2 class="card-title fw-bold mb-3">${information[index].name}</h2>
         </div>
       </div>
     `;
 
-  informationContainer.appendChild(informationSection);
+    informationContainer.appendChild(informationSection);
+  }
 };
 
-for (let index = 1; index < 7; index++) {
-  loadInformation(index);
-}
+loadInformation();
+
+// loadMoreInformation();
