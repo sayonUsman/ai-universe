92b42b036a3fe74fdf3494c7d1662364ae72cddf
Sayon Usman <sayonusman.ma@gmail.com>
2023-03-02 22:42:40 +0300

    Added loadMoreInformation and displayMoreInformation function in js file

--- aiUniverseHub.js
+++ aiUniverseHub.js
@@ -3,8 +3,18 @@ const loadInformation = async () => {
     const url = `https://openapi.programming-hero.com/api/ai/tools`;
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data.data.tools);
     displayInformation(data.data.tools);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const loadMoreInformation = async () => {
+  try {
+    const url = `https://openapi.programming-hero.com/api/ai/tools`;
+    const response = await fetch(url);
+    const data = await response.json();
+    displayMoreInformation(data.data.tools);
   } catch (error) {
     console.log(error);
   }
@@ -15,7 +25,10 @@ const displayInformation = (information) => {
 
   for (let index = 0; index < 6; index++) {
     const informationSection = document.createElement("div");
-    console.log(information[index]);
+
+    if (index === 3) {
+      information[index].features[2] = "Live search";
+    }
 
     informationSection.innerHTML = `
       <div class="card h-100">
@@ -36,6 +49,35 @@ const displayInformation = (information) => {
   }
 };
 
-loadInformation();
+const displayMoreInformation = (information) => {
+  const informationContainer = document.getElementById("information-container");
+  informationContainer.innerHTML = "";
 
-// loadMoreInformation();
+  for (let index = 0; index < information.length; index++) {
+    const informationSection = document.createElement("div");
+
+    if (index === 3) {
+      information[index].features[2] = "Live search";
+    }
+
+    informationSection.innerHTML = `
+      <div class="card h-100">
+        <img src="${information[index].image}" class="img-fluid p-3" alt="There is a image.">
+
+        <div class="card-body">
+          <h2 class="card-title fw-bold mb-3">Features</h2>
+          <p>1. ${information[index].features[0]}</p>
+          <p>2. ${information[index].features[1]}</p>
+          <p>3. ${information[index].features[2]}</p>
+          <hr class="ps-3 pe-3"/>
+          <h2 class="card-title fw-bold mb-3">${information[index].name}</h2>
+        </div>
+      </div>
+    `;
+
+    informationContainer.appendChild(informationSection);
+    document.getElementById("see-btn").innerHTML = "";
+  }
+};
+
+loadInformation();
