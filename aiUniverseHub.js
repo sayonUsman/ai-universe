406c507c495c6cbb4d7d8d0f114278cb439a3171
Sayon Usman <sayonusman.ma@gmail.com>
2023-03-03 13:47:37 +0300

    Removed loadMoreInformation and displayMoreInformation function from js file

--- aiUniverseHub.js
+++ aiUniverseHub.js
@@ -1,29 +1,18 @@
-const loadInformation = async () => {
+const loadInformation = async (length) => {
   try {
     const url = `https://openapi.programming-hero.com/api/ai/tools`;
     const response = await fetch(url);
     const data = await response.json();
-    displayInformation(data.data.tools);
+    displayInformation(data.data.tools, length);
   } catch (error) {
     console.log(error);
   }
 };
 
-const loadMoreInformation = async () => {
-  try {
-    const url = `https://openapi.programming-hero.com/api/ai/tools`;
-    const response = await fetch(url);
-    const data = await response.json();
-    displayMoreInformation(data.data.tools);
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-const displayInformation = (information) => {
+const displayInformation = (information, length = information.length) => {
   const informationContainer = document.getElementById("information-container");
 
-  for (let index = 0; index < 6; index++) {
+  for (let index = 0; index < length; index++) {
     const informationSection = document.createElement("div");
 
     if (index === 3) {
@@ -31,7 +20,7 @@ const displayInformation = (information) => {
     }
 
     informationSection.innerHTML = `
-      <div class="card h-100">
+      <div class="card shadow h-100">
         <img src="${information[index].image}" class="img-fluid p-3" alt="There is a image.">
 
         <div class="card-body">
@@ -53,39 +42,4 @@ const displayInformation = (information) => {
   }
 };
 
-const displayMoreInformation = (information) => {
-  const informationContainer = document.getElementById("information-container");
-  informationContainer.innerHTML = "";
-
-  for (let index = 0; index < information.length; index++) {
-    const informationSection = document.createElement("div");
-
-    if (index === 3) {
-      information[index].features[2] = "Live search";
-    }
-
-    informationSection.innerHTML = `
-      <div class="card h-100">
-        <img src="${information[index].image}" class="img-fluid p-3" alt="There is a image.">
-
-        <div class="card-body">
-          <h2 class="card-title fw-bold mb-3">Features</h2>
-          <p>1. ${information[index].features[0]}</p>
-          <p>2. ${information[index].features[1]}</p>
-          <p>3. ${information[index].features[2]}</p>
-          <hr class="ps-3 pe-3"/>
-
-          <div class="d-flex justify-content-between align-items-center">
-            <h2 class="card-title fw-bold mb-3">${information[index].name}</h2>
-            <button type="button" class="btn rounded-circle" data-bs-toggle="modal" data-bs-target="#detailsModal"><img class="img-fluid" src="arrow.svg" />
-          </div>
-        </div>
-      </div>
-    `;
-
-    informationContainer.appendChild(informationSection);
-    document.getElementById("see-btn").innerHTML = "";
-  }
-};
-
-loadInformation();
+loadInformation(6);
